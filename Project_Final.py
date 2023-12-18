d49a0f71b05fb2c5c8800ad19da825f819f51be6
iNS4L1027 <insal102727@jwu.ac.kr>
2023-12-18 23:28:07 +0900

    Third commit

--- Project_Final.py
+++ Project_Final.py
@@ -1,3 +1,4 @@
+from distutils.core import setup
 import tkinter as tk
 from tkinter import *
 from tkinter.simpledialog import *
@@ -6,6 +7,22 @@ from tkcalendar import DateEntry
 from datetime import datetime, timedelta
 import threading
 import pickle
+
+# 백그라운드 서비스 클래스
+class BackgroundService(threading.Thread):
+    def run(self):
+        while True:
+            current_time = datetime.now()
+
+            for todo in todo_list:
+                notification_time = datetime.combine(todo[1], datetime.min.time()) + timedelta(hours=9)
+                if current_time >= notification_time:
+                    message = f"{todo[0]}의 마감일입니다!"
+                    messagebox.showinfo("할 일 알림", message)
+                    todo_list.remove(todo)
+                    listbox.delete(tk.ACTIVE)
+
+            threading.Event().wait(60)
 
 # 알림 상태 저장
 notification_enabled = True
@@ -22,6 +39,7 @@ def send_notification(time, message):
     if time_diff <= 0:
         messagebox.showinfo("할 일 알림", message)
     else:
+        # 알림 스레드 생성 및 시작
         global notification_thread
         notification_thread = threading.Timer(time_diff, messagebox.showinfo, args=("할 일 알림", message))
         notification_thread.start()
@@ -47,13 +65,13 @@ def add_todo():
     deadline = cal.get_date()
     notification_time = deadline + timedelta(hours=9)
     if todo and deadline:
-        todo_list.append((todo, deadline))
+        todo_list.append((todo, deadline, notification_time))
         listbox.insert(tk.END, f"{todo} (기한: {deadline})")
         entry.delete(0, tk.END)
         cal.set_date(None)
         save_data()
         if notification_enabled:
-            send_notification(str(notification_time), f"{todo}의 마감일입니다.")
+            send_notification(str(notification_time), f"{todo}의 마감일입니다!")
 
 # 할 일 제거 함수
 def delete_todo():
@@ -79,41 +97,50 @@ def load_data():
                 listbox.insert(tk.END, f"{todo} (기한: {deadline})")
     except FileNotFoundError:
         return
+    
+# 백그라운드 서비스 시작
+def start_background_service():
+    background_service = BackgroundService()
+    background_service.start()
 
 # 창 설정
 window = tk.Tk()
 window.title("TO_DO")
-window.geometry("775x695")
+window.geometry("915x755")
 window.resizable(width=False, height=False)
 window.configure(bg="#1C1C1C")
 
 # 날짜 선택
-cal = DateEntry(window, width=12, background='#424242',
-                foreground='white', borderwidth=2)
-cal.grid(row=0, column=0)
+cal = DateEntry(window, width=12, background='#424242', foreground='white', borderwidth=2)
+cal.grid(row=0, column=0, padx=10, pady=10, sticky="W")
 
 # 할 일 입력 칸
 entry = tk.Entry(window, font="맑은고딕, 15", background="#424242", fg="White", width=50)
-entry.grid(row=0, column=1)
+entry.grid(row=0, column=1, padx=10, pady=10)
 
+#------------------------------------------------------------------------------버튼------------------------------------------------------------------------------#
 
 # 할 일 추가 버튼
 add_button = tk.Button(window, text="추가", bg="#424242", font="맑은고딕, 13", fg="White", command=add_todo)
-add_button.grid(row=0, column=2)
-
+add_button.grid(row=0, column=2, padx=10, pady=10, sticky="E")
 
 # 할 일 목록 표시 리스트박스
 listbox = tk.Listbox(window, font="맑은고딕, 15", background="#424242", fg="White", height=30, width=70)
-listbox.grid(row=1, column=0, columnspan=3)
-
+listbox.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
 # 할 일 삭제 버튼
 delete_button = tk.Button(window, text="삭제", bg="#424242", font="맑은고딕, 13", fg="White", command=delete_todo)
-delete_button.grid(row=2, column=0, columnspan=2)
+delete_button.grid(row=2, column=0, padx=10, pady=10, sticky="W")
 
 # 알림 설정 온/오프 버튼
 toggle_button = tk.Button(window, text="알림 끄기", bg="#424242", font="맑은고딕, 13", fg="White", command=toggle_notification)
-toggle_button.grid(row=2, column=2, columnspan=2)
+toggle_button.grid(row=2, column=1, padx=10, pady=10)
+
+# 백그라운드 서비스 버튼
+start_service_button = tk.Button(window, text="백그라운드 서비스 시작", bg="#424242", font="맑은고딕, 13", fg="White", command=start_background_service)
+start_service_button.grid(row=2, column=2, padx=10, pady=10, sticky="E")
+
+#------------------------------------------------------------------------------마무리-----------------------------------------------------------------------------#
 
 # 데이터 로드
 load_data()
